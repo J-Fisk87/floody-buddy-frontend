c0f29cef5102fd483ba49b95e56b8f2c1763ef2f
J-Fisk87 <Jeff.Fisk512@gmail.com>
2020-05-07 22:13:31 -0500

    can add gauges to users DOM/db

--- src/map.js
+++ src/map.js
@@ -1,10 +1,32 @@
 console.log(".js party")
 
 const map = document.getElementById("map");
+// let g;
+let e;
 
 
 function doThing(event){
     e = event.target.alt
     console.log(event)
+    let g = gauges.find(gauge => 
+        gauge.location == event.target.alt)
+    userGaugesPost(g);
+    currentUser.gauges.push(g)
+    displayUserLocations();
+};
+
+function userGaugesPost(g) {
+    fetch(userGaugesUrl, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            gauge_id: g.id,
+            user_id: currentUser.id
+        })
+    }).then(r => r.json())
+        .then(data => console.log(data))
 
 }
